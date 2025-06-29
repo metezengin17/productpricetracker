51630ad9e2c57d44b44865b083f77beeb1a28aa2
metezengin17 <metezengin17@gmail.com>
2025-06-29 14:46:24 +0300

    yeni sürüm

--- main.py
+++ main.py
@@ -29,20 +29,38 @@ trenydol_close_button = WebDriverWait(driver, 10).until(
 trenydol_close_button.click()
 
 trenydol_input_element = driver.find_element(By.CSS_SELECTOR, "input[data-testid='suggestion']")
-trenydol_input_element.send_keys("champion tshirt")
+trenydol_input_element.send_keys("Apple iphone 16 1tb")
 
 trenydol_search_element = driver.find_element(By.CSS_SELECTOR, "i[data-testid='search-icon']")
 trenydol_search_element.click()
 
-last_height = driver.execute_script("return document.body.scrollHeight")
 
-import time
+filter_dropdown = driver.find_element(By.XPATH, '//*[@id="search-app"]/div/div/div/div[2]/div[1]/div[2]/div/div')
+filter_dropdown.click()
 
-scroll_pause_time = 1  # scrolllar arası bekleme süresi
-scroll_amount = 500    # her seferinde kaç px kaydırılacak
-
-
-WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "prdct-cntnr-wrppr"))
+lowest_price_option = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, '//*[@id="search-app"]/div/div/div/div[2]/div[1]/div[2]/div/ul/li[2]'))
 )
+lowest_price_option.click()
+time.sleep(1)
+
+products_container = driver.find_element(By.XPATH, '//*[@id="search-app"]/div/div/div/div[2]/div[4]/div[1]/div')
+product_cards = products_container.find_elements(By.CLASS_NAME, 'p-card-wrppr')
+
+
+first_product = product_cards[0]  # En düşük fiyatlı ürün
+product_name = first_product.find_element(By.CLASS_NAME, 'prdct-desc-cntnr').text
+try:
+    product_price = first_product.find_element(By.CSS_SELECTOR, 'div.basket-price-original-wrapper > div.price-item.basket-price-original').text
+except:
+    product_price = first_product.find_element(By.CLASS_NAME, 'price-information').text
+
+print(f"En ucuz ürün: {product_name}")
+print(f"Fiyatı: {product_price}")
+
+
+
+
+
+
 
