fd56cd15d4069e8ebd7739eb9d14a63a9dae2dbe
metezengin17 <metezengin17@gmail.com>
2025-07-04 15:12:49 +0300

    yeni sürüm

---
+++ amazon.py
@@ -0,0 +1,23 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+def search_amazon(search_text):
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+    driver.maximize_window()
+    driver.get("https://www.amazon.com.tr")
+
+    results = []
+
+
+    return results
+
+
+
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+from amazon import search_amazon
 from trendyol_run import search_trendyol
 from hepsiburada_run import search_hepsiburada
 
 search_term = "Apple iphone 16 1tb"
-products_trendyol = [] # bunu sonra sil
-products_trendyol = search_trendyol(search_term)
-products_hepsiburada = search_hepsiburada(search_term)
 
-all_products = products_trendyol + products_hepsiburada
+#products_trendyol = search_trendyol(search_term)
+#products_hepsiburada = search_hepsiburada(search_term)
+products_amazon = search_amazon(search_term)
 
-for p in all_products:
-    print(f"Ürün: {p['name']} - Fiyat: {p['price']}")
+#all_products = products_trendyol + products_hepsiburada + products_amazon
+
+#for p in all_products:
+#    print(f"Ürün: {p['name']} - Fiyat: {p['price']}")
 
 
